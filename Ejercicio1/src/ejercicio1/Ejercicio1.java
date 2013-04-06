b542801acf55cb298a974d944e29b3fd72462d14
Diego M. Rio <dmrio68@gmail.com>
2013-04-06 13:56:37 +0200

    pruebas Ejercicio1
    Primeras pruebas realizadas en el algoritmo del Ejercicio 1

--- Ejercicio1/src/ejercicio1/Ejercicio1.java
+++ Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -1,18 +1,32 @@
 package ejercicio1;
 
+import java.util.Date;
+
 public class Ejercicio1 {
 
     public static void main(String[] args) {
         
-        int a, b, bAux, m, x;
+        long   inicio;
+	long   fin;
+	long   tiempo;
+        
+        int a=543, b=34, bAux, m=5, x=1;
         bAux = b;
+        
+        inicio = (new Date()).getTime();
         
         while(bAux > 0){
             if((bAux % 2) == 1)
                 x = x * a;
             
             a = a * a;
-            b = b / 2;
+            bAux = bAux / 2;
         }
+        
+        fin = (new Date()).getTime();	
+	tiempo = fin - inicio;
+        
+        System.out.println("Tiempo: "+tiempo+" milisegundos");
+        System.out.println("Resultado: "+x);
     }
 }
