22a54fcd84c78580f1cd19a8f980b662dbabe1f1
Diego M. Rio <dmrio68@gmail.com>
2013-04-08 20:53:54 +0200

    Uso de BigInteger Ejercicio3
    Se ha sustituido el tipo de dato "int" por BigInteger en Ejercicio3
    (Practica1)

--- Ejercicio3/src/ejercicio3/Ejercicio3.java
+++ Ejercicio3/src/ejercicio3/Ejercicio3.java
@@ -1,20 +1,49 @@
 package ejercicio3;
 
+import java.math.BigInteger;
+
 public class Ejercicio3 {
+    
+    private static BigInteger inversoZn(BigInteger a, BigInteger n) {
+        BigInteger res = new BigInteger("0");
+        
+        
+        return res;
+    }
 
     public static void main(String[] args) {
-        int[] a = new int[3];
-        int[] m = new int[3];
-        int resM=1, resA=0;
-        a[0] = 1; 
-        a[1] = 2;
-        a[2] = 3;
-        m[0] = 2; 
-        m[1] = 5;
-        m[2] = 7;
+        BigInteger[] a = new BigInteger[3];
+        BigInteger[] m = new BigInteger[3];
+        BigInteger[] invA = new BigInteger[3];
+        BigInteger resM = new BigInteger("1");
+        BigInteger resA = new BigInteger("0");
+        
+        a[0] = new BigInteger("1"); 
+        a[1] = new BigInteger("2"); 
+        a[2] = new BigInteger("3"); 
+        m[0] = new BigInteger("2"); 
+        m[1] = new BigInteger("5"); 
+        m[2] = new BigInteger("7"); 
+        
+        
+        
+        for(int i = 0; i < 3; i++)
+            resM = resM.multiply(m[i]);
+        
+        for(int i = 0; i < 3; i++)
+            invA[i] = inversoZn(a[i], m[i]);
         
         for(int i = 0; i < 3; i++){
-            resM = resM*m[i];
+            System.out.println("Multiplicamos: "+a[i]+"*"+resM.divide(m[i])+"*"+a[i].modInverse(m[i]));
+            resA = resA.add(a[i].multiply((resM.divide(m[i]))).multiply(a[i].modInverse(m[i])));
+           
+            
         }
+        
+        //resA = resA.mod(resM);
+        
+        System.out.println("La congruencia resultado es: x="+resA+"(mod "+resM+")");
     }
+
+    
 }
