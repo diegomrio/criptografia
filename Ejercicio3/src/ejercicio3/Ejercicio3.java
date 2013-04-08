1c6ca35238340291b1928f6d5534df0a7d3c869c
Diego M. Rio <dmrio68@gmail.com>
2013-04-08 14:10:14 +0200

    Primer esbozo algoritmo Ejercicio3
    Primera aproximacion del algoritmo del Ejercicio3. Realizado con datos
    tipo "int"

--- Ejercicio3/src/ejercicio3/Ejercicio3.java
+++ Ejercicio3/src/ejercicio3/Ejercicio3.java
@@ -3,6 +3,18 @@ package ejercicio3;
 public class Ejercicio3 {
 
     public static void main(String[] args) {
+        int[] a = new int[3];
+        int[] m = new int[3];
+        int resM=1, resA=0;
+        a[0] = 1; 
+        a[1] = 2;
+        a[2] = 3;
+        m[0] = 2; 
+        m[1] = 5;
+        m[2] = 7;
         
+        for(int i = 0; i < 3; i++){
+            resM = resM*m[i];
+        }
     }
 }
