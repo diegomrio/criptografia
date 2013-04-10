6b17d1793de3bb47373531b1025b2549cc3d44f3
Diego M. Rio <dmrio68@gmail.com>
2013-04-10 17:51:36 +0200

    Pruebas Ejercicio 3
    Pruebas realizadas controlando el tiempo

--- Ejercicio3/src/ejercicio3/Ejercicio3.java
+++ Ejercicio3/src/ejercicio3/Ejercicio3.java
@@ -1,46 +1,41 @@
 package ejercicio3;
 
 import java.math.BigInteger;
+import java.util.Date;
 
 public class Ejercicio3 {
     
-    private static BigInteger inversoZn(BigInteger a, BigInteger n) {
-        BigInteger res = new BigInteger("0");
-        
-        
-        return res;
-    }
-
     public static void main(String[] args) {
+        long   inicio;
+	long   fin;
+	long   tiempo;
+        
         BigInteger[] a = new BigInteger[3];
         BigInteger[] m = new BigInteger[3];
-        BigInteger[] invA = new BigInteger[3];
         BigInteger resM = new BigInteger("1");
         BigInteger resA = new BigInteger("0");
         
-        a[0] = new BigInteger("1"); 
-        a[1] = new BigInteger("2"); 
-        a[2] = new BigInteger("3"); 
-        m[0] = new BigInteger("2"); 
-        m[1] = new BigInteger("5"); 
-        m[2] = new BigInteger("7"); 
-        
-        
+        a[0] = new BigInteger("1654888886341"); 
+        a[1] = new BigInteger("2256484312548"); 
+        a[2] = new BigInteger("6548741236458"); 
+        m[0] = new BigInteger("46381"); 
+        m[1] = new BigInteger("768479"); 
+        m[2] = new BigInteger("9476407"); 
+            
+        inicio = (new Date()).getTime();
         
         for(int i = 0; i < 3; i++)
             resM = resM.multiply(m[i]);
         
         for(int i = 0; i < 3; i++)
-            invA[i] = inversoZn(a[i], m[i]);
+            resA = resA.add(a[i].multiply((resM.divide(m[i]))).multiply(resM.divide(m[i]).modInverse(m[i])));
         
-        for(int i = 0; i < 3; i++){
-            System.out.println("Multiplicamos: "+a[i]+"*"+resM.divide(m[i])+"*"+a[i].modInverse(m[i]));
-            resA = resA.add(a[i].multiply((resM.divide(m[i]))).multiply(a[i].modInverse(m[i])));
-           
-            
-        }
+        resA = resA.mod(resM);
         
-        //resA = resA.mod(resM);
+        fin = (new Date()).getTime();	
+	tiempo = fin - inicio;
+        
+        System.out.println("Tiempo: "+tiempo+" segundos");
         
         System.out.println("La congruencia resultado es: x="+resA+"(mod "+resM+")");
     }
